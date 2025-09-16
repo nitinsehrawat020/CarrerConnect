c8f91c86fc91ed05423671775d4829f24f83b2eb
nitinsehrawat020 <nitinsehrawat020@gmail.com>
2025-09-16 16:58:18 +0530

    resume fixed

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,9 +1,9 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
-import { meetingsProcessing } from "@/inngest/functions";
+import { meetingsProcessing, resumeProcessing } from "@/inngest/functions";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [meetingsProcessing],
+  functions: [meetingsProcessing, resumeProcessing],
 });
